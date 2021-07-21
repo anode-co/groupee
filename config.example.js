2de5bee87888558e44fa80c6036c14a9d99d21d2
Thierry Marianne <thierry@marianne.io>
2021-07-21 12:04:03 +0200

    Ensure only administrators can send direct commands to bot account

--- config.example.js
+++ config.example.js
@@ -1,9 +1,37 @@
+
+const welcomeMessage = `
+Hey {{ screen_name }}!
+
+Welcome to this MatterFOSS instance.
+
+The main channels are:
+{{ main_channels_names }}
+
+Please find the server rules below:
+ - rule 1
+ - rule 2
+ - ...
+
+Abuses can be reported by email at {{ report_abuse_email }}
+`;
+
+const reportAbuseEmail = 'report-abuse@example.com';
+
+const mainChannelsNames = [
+    'Off-Topic',
+    'Town Square'
+];
+
 module.exports = {
     me: 'groupee', // name of the bot
     email: 'email_address_of_the_bot_user',
     passwd: 'bot_password',
+    token: 'personal token, can be used instead of email + passwd',
     server: 'your.mattermost.server.tld',
     team: 'yourteam',
     wssPort: 443,
     httpPort: 80,
+    welcomeMessage,
+    mainChannelsNames,
+    reportAbuseEmail
 };
--- groupee.js
+++ groupee.js
@@ -63,26 +63,38 @@ type Context_t = {
     mm: any,
     cfg: any,
     mut: {
-        bot_id: string
-    }
+        botId: string,
+        systemAdministrators: Array<any>
+    },
+    debug: (...arg: any[]) => void,
+    info: (...arg: any[]) => void,
+    error: (...arg: any[]) => void,
+    backtrace: () => void
 }
 */
 
+const chainError = err => Promise.reject(err);
 
-const userName = ({ctx, userId}) => {
+const getUsernameByUserId = ({ctx, userId}) => {
     ctx = (ctx /*:Context_t*/);
     const u = ctx.mm.users[userId];
     return u ? u.username : `user:${userId}`;
 };
 
 const reply = (ctx, reply, m /*:Message_t*/) => {
+    if (m.data.sender_name === undefined) {
+        ctx.info(`in reaction to system event: ${reply}`);
+        return;
+    }
+
+    ctx.info(`${m.data.sender_name}: ${reply}`);
     ctx.mm.postMessage(m.data.sender_name + ' ' + reply, m.broadcast.channel_id);
 };
 
 const apiCall = (ctx, method, path, post, then) => {
-    console.error('apiCall', method, path);
+    ctx.info('API call', method, path);
     ctx.mm._apiCall(method, path, post, (data, headers) => {
-        console.error('apiCall', method, path, ' -- done');
+        ctx.info('API call', method, path, ' -- done');
         return then(data, headers);
     });
 };
@@ -106,7 +118,7 @@ const isValidUserIdFormat = (userId) => {
 };
 
 const makeApiCallPromise = (ctx, uri, method, then, params = null) /*: Promise<any>*/ => {
-    return new Promise((resolve, reject) => {
+    return (new Promise((resolve, reject) => {
         try {
             apiCall(
                 ctx,
@@ -118,7 +130,7 @@ const makeApiCallPromise = (ctx, uri, method, then, params = null) /*: Promise<a
         } catch (e) {
             reject(e);
         }
-    });
+    }));
 };
 
 const findTeamByName = (ctx /*:Context_t*/) /*: Promise<any>*/ => {
@@ -135,7 +147,7 @@ const getMainChannelsNames = (ctx /*:Context_t*/, teamId) /*: Promise<any>*/ => 
         ctx,
         `/teams/${teamId}/channels`,
         'GET',
-        (mainChannels, resolve) => {
+        (mainChannels, resolve, _, __) => {
             if (mainChannels.length === 0) {
                 throw 'No main channel name has been defined. See config.js.';
             }
@@ -152,8 +164,9 @@ const searchUserByTerm = (ctx, term, page = 0) => {
         '/users/search',
         'POST',
         (users, resolve, _, reject) => {
-            if (!Array.isArray(users) || users.length !== 1) {
-                reject(`Ambiguous command. Can not find user by "${term}" term`);
+            if (!Array.isArray(users) || users.length < 1) {
+                reject(`Ambiguous command. Can not find single user by term: "${term}"`);
+                return;
             }
 
             resolve(users[0]);
@@ -166,12 +179,39 @@ const searchUserByTerm = (ctx, term, page = 0) => {
     );
 };
 
+const findSystemAdministrators = (ctx, page = 0, perPage = 200 ) => {
+    return makeApiCallPromise(
+        ctx,
+        `/users?page=${page}&per_page=${perPage}&role=system_admin`,
+        'GET',
+        (systemAdministrators, resolve, _, reject) => {
+            if (
+                !Array.isArray(systemAdministrators) ||
+                systemAdministrators.length < 1
+            ) {
+                reject(`There are no system administrators`);
+                return;
+            }
+
+            if (
+                systemAdministrators
+                .find(a => a.id === ctx.mut.botId) === undefined
+            ) {
+                reject(`The configured bot account (see config.js) is not an administrator.`);
+                return;
+            }
+
+            resolve(systemAdministrators);
+        }
+    );
+};
+
 const demoteUserHavingUserId = (ctx /*:Context_t*/, userId /*:string */, then) /*: Promise<any>*/ => {
     return makeApiCallPromise(
         ctx,
         `/users/${userId}/demote`,
         'POST',
-        (result, resolve, headers) => {
+        (result, resolve, headers, _) => {
             resolve(result);
 
             if (typeof then === 'function') {
@@ -181,49 +221,101 @@ const demoteUserHavingUserId = (ctx /*:Context_t*/, userId /*:string */, then) /
     );
 };
 
+const postMessage = (
+    ctx /*:Context_t*/,
+    message /* string */,
+    channelId /* string*/,
+    then /*: void|(data: any, resolve: () => void, headers: any, reject: () => void) => void */
+) /*: Promise<any> */ => {
+    return makeApiCallPromise(
+        ctx,
+        '/posts',
+        'POST',
+        (postedMessage, resolve, headers, reject) => {
+            if (typeof then === 'function') {
+                then(postedMessage, resolve, headers, reject);
+                return;
+            }
+
+            ctx.info({ postedMessage, headers });
+            resolve(postedMessage);
+        },
+        {
+            message,
+            file_ids: [],
+            create_at: 0,
+            user_id: ctx.mut.botId,
+            channel_id: channelId
+        }
+    );
+};
+
 const formatWelcomeMessage = (message, params) => {
     return message
         .replace(
             '{{ screen_name }}',
-            `${params.screen_name}`
+            params.screen_name
         )
         .replace(
             '{{ main_channels_names }}',
             params.main_channels_names
             .map(name => `- ~${name}`)
             .join("\n")
+        )
+        .replace(
+            '{{ report_abuse_email }}',
+            params.report_abuse_email
         );
+};
+
+const offerDialogToAcceptRulesOrRejectThem = (ctx, channelId) => {
+    return postMessage(ctx, '/me Do you accept the rules or not?', channelId);
 };
 
 const postWelcomeMessage = (ctx, mainChannelsNames, userId) => {
     return new Promise((resolve, reject) => {
         try {
-            ctx.mm.getUserDirectMessageChannel(userId, channel => {
-                const formattedMessage = formatWelcomeMessage(
-                    ctx.cfg.welcomeMessage,
-                    {
-                        screen_name: userName({ctx, userId}),
-                        main_channels_names: mainChannelsNames
-                    }
-                );
+            ctx.mm.getUserDirectMessageChannel(
+                userId,
+                channel => {
+                    const formattedMessage = formatWelcomeMessage(
+                        ctx.cfg.welcomeMessage,
+                        {
+                            screen_name: getUsernameByUserId({ctx, userId}),
+                            main_channels_names: mainChannelsNames,
+                            report_abuse_email: ctx.cfg.report_abuse_email
+                        }
+                    );
 
-                ctx.mm.postMessage(formattedMessage, channel.id);
-                resolve(
-                    `Welcome message was successfully posted to user having id ${userId}`
-                );
-            });
+                    postMessage(ctx, formattedMessage, channel.id)
+                    .then(() => {
+                        resolve(`Welcome message was successfully posted to user having id ${userId} in channel with id ${channel.id}`);
+                    }, chainError)
+                    .then(() => offerDialogToAcceptRulesOrRejectThem(ctx), chainError);
+                }
+            );
         } catch (e) {
             reject(e);
         }
     });
 };
 
-const runWelcomeFlow = (ctx /*:Context_t*/, userId /*:string */, m /*:Message_t*/)/*: Promise<any> */ => {
-    return demoteUserHavingUserId(ctx, userId)
-    .then(() => findTeamByName(ctx))
-    .then(({id: teamId}) => getMainChannelsNames(ctx, teamId))
-    .then(mainChannelsNames => postWelcomeMessage(ctx, mainChannelsNames, userId))
-    .then(success => reply(ctx, success, m));
+const runWelcomeFlow = (ctx /*:Context_t*/, userId /*:string */, m /*:Message_t*/)/*: Promise<any>|void */ => {
+    return findSystemAdministrators(ctx)
+    .then((systemAdministrators) => {
+        ctx.mut.systemAdministrators = systemAdministrators;
+        ctx.debug('System administrators are', systemAdministrators);
+
+        return demoteUserHavingUserId(ctx, userId);
+    }, chainError)
+    .then(() => findTeamByName(ctx), chainError)
+    .then(({id: teamId}) => getMainChannelsNames(ctx, teamId), chainError)
+    .then(mainChannelsNames => postWelcomeMessage(ctx, mainChannelsNames, userId), chainError)
+    .then(success => reply(ctx, success, m), chainError)
+    .catch(e => {
+        ctx.error(e);
+        reply(ctx, `Could not run welcome flow with error: \n${e}`, m);
+    });
 };
 
 const wel = (ctx /*:Context_t*/, words /*:Array<string>*/, m /*:Message_t*/) => {
@@ -248,27 +340,28 @@ const wel = (ctx /*:Context_t*/, words /*:Array<string>*/, m /*:Message_t*/) => 
         username = userIdOrUsername.substr(1, userIdOrUsername.length - 1);
     }
 
-    searchUserByTerm(ctx, `${username}`)
+    return searchUserByTerm(ctx, `${username}`)
     .then(({id: userId}) => {
         if (!isValidUserIdFormat(userId)) {
             throw `Could not find user id from term ${username}`;
         }
 
         return runWelcomeFlow(ctx, userId, m);
+    }, chainError)
+    .catch(e => {
+        reply(ctx, `Could not execute welcome command with error: \n${e}`, m);
     });
 };
 
 let COMMANDS;
-let COMMANDS_PVT;
+
 const help = (ctx /*:Context_t*/, _words /*:Array<string>*/, m /*:Message_t*/) => {
     let out = [`To send a command, use: !<command> <arg1> <arg2> ...`];
+
     for (const c in COMMANDS) {
         out.push(`**!${c}**: ${COMMANDS[c].help}`);
     }
-    out.push(`\nIn a private chat with me, you can also use the commands:`);
-    for (const c in COMMANDS_PVT) {
-        out.push(`**!${c}**: ${COMMANDS_PVT[c].help}`);
-    }
+
     out.push('For more information, see: https://github.com/anode-co/groupee/blob/master/readme.md');
     reply(ctx, out.join('\n'), m);
 };
@@ -279,53 +372,81 @@ COMMANDS = {
 };
 
 const message = (ctx, m /*:Message_t*/) => {
-    if (m._post.user_id === ctx.mut.bot_id) { return; }
-    const words = m._post.message.split(' ').filter((w) => w !== '');
-    if (words.length < 1) {
-        console.log('empty message');
-        return;
-    }
+    findSystemAdministrators(ctx)
+    .then((systemAdministrators) => {
+        ctx.mut.systemAdministrators = systemAdministrators;
+        ctx.debug('System administrators are', systemAdministrators);
+        if (!m._channel || typeof m._channel.name !== 'string') {
+            return;
+        }
 
-    const isDirectMessageChannel = (m._channel.type === 'D');
-    const bang = (words[0].indexOf('!') === 0);
-    const cmdName = words.shift().replace(/^!/, '');
+        // @see https://github.com/thierrymarianne/contrib-matterfoss/blob/99b26af24bf63be55acc3ba0e67843a0a36506e0/model/channel.go#L354-L359
+        const directChannelUserIds = m.data.channel_name.split('__');
 
-    let c = COMMANDS[cmdName];
-    if (bang && c) {
-    } else {
-        if (isDirectMessageChannel) { reply(ctx, "I don't understand what you said, use !help for a list commands", m); }
-        return;
-    }
+        if (ctx.mut.systemAdministrators.length < 1) {
+            ctx.error('System administrators should be known before replying to reacting to events.');
+            return;
+        }
 
-    if (Array.isArray(c.argc)) {
-        let ok = false;
-        for (const count of c.argc) {
-            if (words.length === count) {
-                ok = true;
+        if (
+            directChannelUserIds
+            .some(
+                userId => ctx.mut.systemAdministrators.find(a => a.id === userId) === undefined
+            )
+        ) {
+            ctx.error('Only system administrators can send commands to bot account.');
+            return;
+        }
+
+        if (m._post.user_id === ctx.mut.botId) { return; }
+        const words = m._post.message.split(' ').filter((w) => w !== '');
+        if (words.length < 1) {
+            ctx.info('empty message');
+            return;
+        }
+
+        const isDirectMessageChannel = (m._channel.type === 'D');
+        const bang = (words[0].indexOf('!') === 0);
+        const cmdName = words.shift().replace(/^!/, '');
+
+        let c = COMMANDS[cmdName];
+        if (bang && c) {
+        } else {
+            if (isDirectMessageChannel) { reply(ctx, "I don't understand what you said, use !help for a list commands", m); }
+            return;
+        }
+
+        if (Array.isArray(c.argc)) {
+            let ok = false;
+            for (const count of c.argc) {
+                if (words.length === count) {
+                    ok = true;
+                }
             }
+            if (!ok) {
+                return void reply(ctx, `Error: ${cmdName} requires ${c.argc.join(' or ')} arguments (${c.help})`, m);
+            }
+        } else if (c.argc < 0) {
+            if (words.length < -c.argc) {
+                return void reply(ctx, `Error: ${cmdName} requires at least ${c.argc} arguments (${c.help})`, m);
+            }
+        } else if (words.length !== c.argc) {
+            return void reply(ctx, `Error: ${cmdName} requires ${c.argc} arguments (${c.help})`, m);
         }
-        if (!ok) {
-            return void reply(ctx, `Error: ${cmdName} requires ${c.argc.join(' or ')} arguments (${c.help})`, m);
-        }
-    } else if (c.argc < 0) {
-        if (words.length < -c.argc) {
-            return void reply(ctx, `Error: ${cmdName} requires at least ${c.argc} arguments (${c.help})`, m);
-        }
-    } else if (words.length !== c.argc) {
-        return void reply(ctx, `Error: ${cmdName} requires ${c.argc} arguments (${c.help})`, m);
-    }
-    return void c.cmd(ctx, words, m);
+
+        return void c.cmd(ctx, words, m);
+    }, chainError);
 };
 
 const connect = (ctx) => {
     ctx.mm.login(ctx.cfg.email, ctx.cfg.passwd);
 
     ctx.mm.on('loggedIn', () => {
-        console.log('logged in');
+        ctx.info('logged in');
     });
 
     ctx.mm.on('meLoaded', (me) => {
-        ctx.mut.bot_id = me.id;
+        ctx.mut.botId = me.id;
     });
 
     ctx.mm.on('message', (m) => {
@@ -337,7 +458,7 @@ const connect = (ctx) => {
             try {
                 obj = JSON.parse(m.data.post);
             } catch (e) {
-                console.log('failed to parse', m);
+                ctx.info('failed to parse', m);
                 return;
             }
             if (typeof obj.id !== 'string') {
@@ -360,7 +481,7 @@ const connect = (ctx) => {
                 m._post = obj;
                 return void channelById(ctx, m._post.channel_id, (err, ret) => {
                     if (err) {
-                        console.error('channelById', err);
+                        ctx.error('channelById', err);
                         return;
                     }
                     m._channel = ret;
@@ -368,14 +489,15 @@ const connect = (ctx) => {
                 });
             }
         }
-        console.log('unexpected message structure', m);
+
+        ctx.info('unexpected message structure', m);
     });
 
     ctx.mm.on('raw_message', (m) => {
         if (m.event === 'new_user') {
             if (!m.data) {
             } else if (!isValidUserIdFormat(m.data.user_id)) {
-                console.error('Invalid user id');
+                ctx.error('Invalid user id');
                 return;
             }
 
@@ -387,19 +509,44 @@ const connect = (ctx) => {
 const main = (config) => {
     process.env.MATTERMOST_LOG_LEVEL = process.env.MATTERMOST_LOG_LEVEL || 'notice';
 
+    const client = new Mattermost(
+        config.server,
+        config.team,
+        {
+            wssPort: config.wssPort,
+            httpPort: config.httpPort
+        }
+    );
+
+    const logPrefix = 'GROUPEE';
+
+    const debug = (...args) => {
+        client.logger.debug(logPrefix, ...args);
+    };
+
+    const info = (...args) => {
+        client.logger.info(logPrefix, ...args);
+    };
+
+    const error = (...args) => {
+        client.logger.error(logPrefix, ...args);
+    };
+    
+    const backtrace = () => {
+        info({stack: (new Error()).stack});
+    };
+
     const ctx = Object.freeze({
-        mm: new Mattermost(
-            config.server,
-            config.team,
-            {
-                wssPort: config.wssPort,
-                httpPort: config.httpPort
-            }
-        ),
+        mm: client,
         cfg: config,
         mut: {
-            bot_id: '_'
-        }
+            botId: '_',
+            systemAdministrators: []
+        },
+        debug,
+        info,
+        error,
+        backtrace
     });
     connect(ctx);
 };
